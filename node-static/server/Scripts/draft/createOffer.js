f0363fd368fa1ed38d0d87699adebf42edb90141
Горшков Иван <gorshkov.apress@gmail.com>
2016-05-06 10:56:56 +0500

    добавил проект

---
+++ node-static/server/Scripts/draft/createOffer.js
@@ -0,0 +1,35 @@
+﻿createOffers();
+function createOffers() {
+
+  $.ajax({
+    type: 'GET',
+    url: 'offers.json',
+    success: function (data) {
+
+       var
+        source,
+        template;
+
+      source = $('#offer-plate-templ').html();
+      template = Handlebars.compile(source);
+      var ss = template(data);
+
+      var columns = $('.column');
+      var columnACountOffers = [];
+
+      var index = 0;
+
+      while (index < data.offers.length) {
+        jQuery.each(columns, function (i) {
+          if (index >= data.offers.length) {
+            return false;
+          }
+          columnACountOffers[i] = $(this).find('.offer-plate').length;
+
+          $(this).append(template(data.offers[index]));
+          index += 1;
+        });
+      }
+    }
+  });
+}
---
+++ node-static/server/server.js
@@ -0,0 +1,29 @@
+﻿var http = require('http');
+var url = require('url');
+var querystring = require('querystring');
+var static = require('node-static');
+var file = new static.Server('.');
+
+
+function accept(req, res) {
+
+  if (req.url == '/vote') {
+    setTimeout(function () {
+      res.end('Ваш голос принят: ' + new Date());
+    }, 1500);
+  } else {
+    file.serve(req, res); // (если он есть)
+  }
+
+}
+
+if (!module.parent) {
+  http.createServer(accept).listen(8081);
+} else {
+  exports.accept = accept;
+}
+
+/*
+fs=require("fs")
+fs.writeFileSync("txt.txt", "my text",  "ascii")
+*/
